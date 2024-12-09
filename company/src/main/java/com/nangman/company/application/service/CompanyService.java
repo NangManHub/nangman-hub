afbe0cbe007401ed4654117f35124cdcd011ace1
zoohee <jjhh1545@gmail.com>
2024-12-10 01:34:44 +0900

    feat: Company 생성 API 구현 #11
    Controller - Service - Repository 로직 구현 및 DB insert 확인

--- company/src/main/java/com/nangman/company/application/service/CompanyService.java
+++ company/src/main/java/com/nangman/company/application/service/CompanyService.java
@@ -1,4 +1,19 @@
 package com.nangman.company.application.service;
 
+import com.nangman.company.application.dto.request.CompanyPostRequest;
+import com.nangman.company.application.dto.response.CompanyPostResponse;
+import com.nangman.company.domain.entity.Company;
+import com.nangman.company.presentation.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyPostResponse createCompany(CompanyPostRequest request) {
+        Company company = companyRepository.save(request.toEntity());
+        return CompanyPostResponse.from(company);
+    }
 }
--- company/src/main/java/com/nangman/company/presentation/CompanyController.java
+++ company/src/main/java/com/nangman/company/presentation/CompanyController.java
@@ -1,4 +1,26 @@
 package com.nangman.company.presentation;
 
+import com.nangman.company.application.dto.request.CompanyPostRequest;
+import com.nangman.company.application.dto.response.CompanyPostResponse;
+import com.nangman.company.application.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/companies")
 public class CompanyController {
+    private final CompanyService companyService;
+
+    @PostMapping
+    public ResponseEntity<CompanyPostResponse> createCompany(
+            @RequestBody CompanyPostRequest request) {
+        // TODO: 접근 권한 제어 로직 추가
+        return ResponseEntity.ok(companyService.createCompany(request));
+    }
+
 }
