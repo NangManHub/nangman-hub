c22d74e393c62a2698ff4a065daf999324a58bc1
POKUDING <yoseb95@gmail.com>
2024-12-09 22:35:36 +0900

    feat: Delivery Repository 생성 #8

---
+++ delivery/src/main/java/com/nangman/delivery/domain/repository/DeliveryRepository.java
@@ -0,0 +1,10 @@
+package com.nangman.delivery.domain.repository;
+
+import com.nangman.delivery.domain.entity.Delivery;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+}
