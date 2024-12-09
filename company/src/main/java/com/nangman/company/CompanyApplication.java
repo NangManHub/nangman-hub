9417aace4d0abb2f086b717eaed45c60835fd71f
zoohee <jjhh1545@gmail.com>
2024-12-10 01:33:15 +0900

    feat: @EnableJpaAuditing 적용
    BaseTimeEntity 자동 설정을 위해 Application 파일에 어노테이션 적용

--- company/src/main/java/com/nangman/company/CompanyApplication.java
+++ company/src/main/java/com/nangman/company/CompanyApplication.java
@@ -2,7 +2,9 @@ package com.nangman.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CompanyApplication {
 
