cec4f51804420ed059e2232222bc2df3b1c3ec32
POKUDING <yoseb95@gmail.com>
2024-12-10 12:47:53 +0900

    feat: Track update 시 sequence 중복 여부 확인 - tracks 를 같이 저장하기 위해 cascade 옵션 추가 #13

--- delivery/src/main/java/com/nangman/delivery/domain/entity/Delivery.java
+++ delivery/src/main/java/com/nangman/delivery/domain/entity/Delivery.java
@@ -1,5 +1,8 @@
 package com.nangman.delivery.domain.entity;
 
+import com.nangman.delivery.common.exception.DomainException;
+import com.nangman.delivery.common.exception.ExceptionStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,7 +14,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -46,12 +51,11 @@ public class Delivery {
     @Column(name="order_id", updatable = false, nullable = false)
     private UUID orderId;
 
-    @OneToMany(mappedBy = "delivery")
+    @OneToMany(mappedBy = "delivery", cascade = CascadeType.ALL)
     private List<Track> tracks = new ArrayList<>();
 
     @Builder
-    public Delivery(DeliveryStatus status, UUID fromHubId, UUID toHubId, String address, UUID recipient, UUID orderId) {
-        this.status = status;
+    public Delivery(UUID fromHubId, UUID toHubId, String address, UUID recipient, UUID orderId) {
         this.fromHubId = fromHubId;
         this.toHubId = toHubId;
         this.address = address;
@@ -61,6 +65,20 @@ public class Delivery {
     }
 
     public void updateTrack(Track track) {
+        if (this.tracks.stream().anyMatch(t -> t.getSequence() == track.getSequence())) {
+            throw new DomainException(ExceptionStatus.TRACK_SEQUENCE_DUPLICATED);
+        }
         this.tracks.add(track);
+    }
+
+    public void updateTracks(List<Track> tracks) {
+        Set<Integer> sequences = tracks.stream().map(Track::getSequence).collect(Collectors.toSet());
+        tracks.forEach(track -> {
+            if (sequences.contains(track.getSequence())) {
+                throw new DomainException(ExceptionStatus.TRACK_SEQUENCE_DUPLICATED);
+            }
+            sequences.add(track.getSequence());
+        });
+        this.tracks.addAll(tracks);
     }
 }
