fc28fb3cdcb27e715f64c21833f2439872803f8a
POKUDING <yoseb95@gmail.com>
2024-12-10 10:45:07 +0900

    feat: Delivery 생성 API 개발 #9

--- delivery/src/main/java/com/nangman/delivery/application/service/DeliveryService.java
+++ delivery/src/main/java/com/nangman/delivery/application/service/DeliveryService.java
@@ -1,5 +1,7 @@
 package com.nangman.delivery.application.service;
 
+import com.nangman.delivery.application.dto.request.DeliveryPostRequest;
+import com.nangman.delivery.application.dto.response.DeliveryResponse;
 import com.nangman.delivery.domain.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DeliveryService {
     private final DeliveryRepository deliveryRepository;
+
+    public DeliveryResponse createDelivery(DeliveryPostRequest request) {
+        return DeliveryResponse.from(deliveryRepository.save(request.toEntity()));
+    }
 }
--- delivery/src/main/java/com/nangman/delivery/presentation/DeliveryController.java
+++ delivery/src/main/java/com/nangman/delivery/presentation/DeliveryController.java
@@ -1,7 +1,13 @@
 package com.nangman.delivery.presentation;
 
+import com.nangman.delivery.application.dto.request.DeliveryPostRequest;
+import com.nangman.delivery.application.dto.response.DeliveryResponse;
 import com.nangman.delivery.application.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/delivery")
 @RequiredArgsConstructor
 public class DeliveryController {
+
     private final DeliveryService deliveryService;
+
+    @PostMapping
+    public ResponseEntity<DeliveryResponse> createDelivery(@RequestBody DeliveryPostRequest request) {
+        DeliveryResponse response = deliveryService.createDelivery(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
