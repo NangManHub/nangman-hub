0aa0633978a07191420dfeb27ec22d0118a9e65d
권현준 <bonheur9813@naver.com>
2024-12-11 14:58:37 +0900

    feat: SecurityFilterChain 추가 #4

--- user/src/main/java/com/nangman/user/common/config/AuthConfig.java
+++ user/src/main/java/com/nangman/user/common/config/AuthConfig.java
@@ -2,8 +2,11 @@ package com.nangman.user.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AuthConfig {
@@ -13,4 +16,21 @@ public class AuthConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests.anyRequest().permitAll()
+        );
+
+        // 설정된 보안 필터 체인을 반환합니다.
+        return http.build();
+    }
 }
