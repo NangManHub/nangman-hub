7c81cc81fb048b159f9f19aebf192d0c0e79f952
POKUDING <yoseb95@gmail.com>
2024-12-11 17:07:42 +0900

    fix: 불필요한 어노테이션 제거 #7

--- delivery/src/main/java/com/nangman/delivery/domain/repository/DeliveryRepository.java
+++ delivery/src/main/java/com/nangman/delivery/domain/repository/DeliveryRepository.java
@@ -5,6 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
 }
