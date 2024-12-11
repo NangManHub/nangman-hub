81a127eebb065c690165bd52ff6cb90045cc14a4
POKUDING <yoseb95@gmail.com>
2024-12-11 10:28:18 +0900

    feat: 배송 수정 엔드포인트 생성 #15

--- delivery/src/main/java/com/nangman/delivery/presentation/DeliveryController.java
+++ delivery/src/main/java/com/nangman/delivery/presentation/DeliveryController.java
@@ -1,6 +1,7 @@
 package com.nangman.delivery.presentation;
 
 import com.nangman.delivery.application.dto.request.DeliveryPostRequest;
+import com.nangman.delivery.application.dto.request.DeliveryPutRequest;
 import com.nangman.delivery.application.dto.response.DeliveryResponse;
 import com.nangman.delivery.application.service.DeliveryService;
 import java.util.UUID;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,13 @@ public class DeliveryController {
     @GetMapping("/{deliveryId}")
     public ResponseEntity<DeliveryResponse> getDelivery(@PathVariable UUID deliveryId) {
         DeliveryResponse response = deliveryService.getDeliveryByDeliveryId(deliveryId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{deliveryId}")
+    public ResponseEntity<DeliveryResponse> updateDelivery(@PathVariable UUID deliveryId, @RequestBody DeliveryPutRequest request) {
+        DeliveryResponse response = deliveryService.updateDelivery(deliveryId, request);
 
         return ResponseEntity.ok(response);
     }
