a9d3eb4e1f0ee25f2441c595947614bf0fa9b605
POKUDING <yoseb95@gmail.com>
2024-12-11 10:30:30 +0900

    feat: 배송 수정 비즈니스 로직 개발 #15

--- delivery/src/main/java/com/nangman/delivery/application/service/DeliveryService.java
+++ delivery/src/main/java/com/nangman/delivery/application/service/DeliveryService.java
@@ -1,27 +1,44 @@
 package com.nangman.delivery.application.service;
 
 import com.nangman.delivery.application.dto.request.DeliveryPostRequest;
+import com.nangman.delivery.application.dto.request.DeliveryPutRequest;
 import com.nangman.delivery.application.dto.response.DeliveryResponse;
 import com.nangman.delivery.common.exception.ApplicationException;
 import com.nangman.delivery.common.exception.ExceptionStatus;
 import com.nangman.delivery.domain.entity.Delivery;
+import com.nangman.delivery.domain.entity.Track;
 import com.nangman.delivery.domain.repository.DeliveryRepository;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DeliveryService {
     private final DeliveryRepository deliveryRepository;
 
+    @Transactional
     public DeliveryResponse createDelivery(DeliveryPostRequest request) {
         return DeliveryResponse.from(deliveryRepository.save(request.toEntity()));
     }
 
+    @Transactional(readOnly = true)
     public DeliveryResponse getDeliveryByDeliveryId(UUID deliveryId) {
         Delivery delivery = deliveryRepository.findById(deliveryId)
-            .orElseThrow(() -> new ApplicationException(ExceptionStatus.DELIVERY_NOT_FOUND));
+                .orElseThrow(() -> new ApplicationException(ExceptionStatus.DELIVERY_NOT_FOUND));
+        return DeliveryResponse.from(delivery);
+    }
+
+    @Transactional
+    public DeliveryResponse updateDelivery(UUID deliveryId, DeliveryPutRequest request) {
+        Delivery delivery = deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new ApplicationException(ExceptionStatus.DELIVERY_NOT_FOUND));
+
+        delivery.update(request.status(), request.fromHubId(), request.toHubId(), request.address(),
+                request.recipient(), request.orderId());
+
         return DeliveryResponse.from(delivery);
     }
 }
--- delivery/src/main/java/com/nangman/delivery/common/exception/ExceptionStatus.java
+++ delivery/src/main/java/com/nangman/delivery/common/exception/ExceptionStatus.java
@@ -9,7 +9,8 @@ public enum ExceptionStatus {
     DELIVERY_NOT_FOUND(HttpStatus.NOT_FOUND, "d001", "해당 배송 정보를 찾을 수 없습니다."),
 
     //TRACK
-    TRACK_SEQUENCE_DUPLICATED(HttpStatus.BAD_REQUEST, "t001", "중복된 배송 단계입니다."),;
+    TRACK_SEQUENCE_DUPLICATED(HttpStatus.BAD_REQUEST, "t001", "중복된 배송 단계입니다."),
+    TRACK_NOT_FOUND(HttpStatus.NOT_FOUND, "t002","해당 배송 경로를 찾을 수 없습니다.");
 
     private final int status;
     private final String customCode;
