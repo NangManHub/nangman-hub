a56c5c36dc9b0460919d35539df376eefdb465c0
권현준 <bonheur9813@naver.com>
2024-12-11 17:10:20 +0900

    comment: 아이디,비밀번호 정규식 추가 #3

--- user/src/main/java/com/nangman/user/application/dto/request/SignupRequest.java
+++ user/src/main/java/com/nangman/user/application/dto/request/SignupRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record SignupRequest(
+        // TODO: 아이디, 비밀번호 정규식 추가
         @NotBlank
         @Pattern(regexp = "^.{0,10}$", message = "아이디는 최대 10자까지만 가능합니다.")
         String username,
