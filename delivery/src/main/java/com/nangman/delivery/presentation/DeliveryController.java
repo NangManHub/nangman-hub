0735583907f90236f586083dd64622b80a8b575a
POKUDING <yoseb95@gmail.com>
2024-12-13 12:51:27 +0900

    feat: Deliver, Track Controller Swagger 설정 추가 #71

--- delivery/src/main/java/com/nangman/delivery/presentation/DeliveryController.java
+++ delivery/src/main/java/com/nangman/delivery/presentation/DeliveryController.java
@@ -7,6 +7,8 @@ import com.nangman.delivery.application.dto.response.DeliveryResponse;
 import com.nangman.delivery.application.service.DeliveryService;
 import com.nangman.delivery.common.annotation.RoleCheck;
 import com.nangman.delivery.domain.enums.UserRole;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Delivery", description = "배송 API")
 @RestController
 @RequestMapping("/deliveries")
 @RequiredArgsConstructor
@@ -33,6 +36,7 @@ public class DeliveryController {
 
     private final DeliveryService deliveryService;
 
+    @Operation(summary = "배송 생성", description = "배송을 생성합니다. Master 권한만 접근이 가능합니다.")
     @RoleCheck(role = {UserRole.MASTER})
     @PostMapping
     public ResponseEntity<DeliveryResponse> createDelivery(@Valid @RequestBody DeliveryPostRequest request) {
@@ -41,6 +45,7 @@ public class DeliveryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "배송 검색", description = "배송을 검색합니다. Master 미만 권한은 본인이 포함된 리소스만 응답받습니다.")
     @GetMapping
     public ResponseEntity<PagedModel<DeliveryResponse>> searchDelivery(DeliverySearchRequest deliverySearchRequest,
                                                                       @PageableDefault Pageable pageable) {
@@ -49,6 +54,7 @@ public class DeliveryController {
         return ResponseEntity.ok(new PagedModel<>(response));
     }
 
+    @Operation(summary = "배송 조회", description = "배송을 조회합니다. Master 미만 권한은 본인이 포함된 리소스만 조회 가능합니다.")
     @GetMapping("/{deliveryId}")
     public ResponseEntity<DeliveryResponse> getDelivery(@PathVariable UUID deliveryId) {
         DeliveryResponse response = deliveryService.getDeliveryById(deliveryId);
@@ -56,6 +62,7 @@ public class DeliveryController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "배송 수정", description = "배송을 수정합니다. Master 미만 권한은 본인이 포함된 리소스만 수정 가능합니다.")
     @RoleCheck(role = {UserRole.MASTER, UserRole.MANAGER, UserRole.SHIPPER})
     @PutMapping("/{deliveryId}")
     public ResponseEntity<DeliveryResponse> updateDelivery(@PathVariable UUID deliveryId,
@@ -65,6 +72,7 @@ public class DeliveryController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "배송 수정", description = "배송을 수정합니다. Master와 Manager만 접근 가능합니다. Manager는 본인이 포함된 리소스만 수정 가능합니다.")
     @RoleCheck(role = {UserRole.MASTER, UserRole.MANAGER})
     @DeleteMapping("/{deliveryId}")
     public ResponseEntity<Void> deleteDelivery(@PathVariable UUID deliveryId,
--- delivery/src/main/java/com/nangman/delivery/presentation/TrackController.java
+++ delivery/src/main/java/com/nangman/delivery/presentation/TrackController.java
@@ -7,6 +7,8 @@ import com.nangman.delivery.application.dto.response.TrackResponse;
 import com.nangman.delivery.application.service.TrackService;
 import com.nangman.delivery.common.annotation.RoleCheck;
 import com.nangman.delivery.domain.enums.UserRole;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +25,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Track", description = "배송 경로 API")
 @RestController
 @RequestMapping("/deliveries/tracks")
 @RequiredArgsConstructor
 public class TrackController {
     private final TrackService trackService;
 
+    @Operation(summary = "배송 경로 수정", description = "배송 경로를 수정합니다. Master만 수정 가능합니다.")
     @RoleCheck(role = {UserRole.MASTER})
     @PutMapping("/{trackId}")
     public ResponseEntity<TrackResponse> updateTrack(@PathVariable UUID trackId,
@@ -38,6 +42,7 @@ public class TrackController {
         return ResponseEntity.ok(trackResponse);
     }
 
+    @Operation(summary = "배송 경로 검색", description = "배송 경로를 검색합니다. Master 미만 권한은 본인이 포함된 리소스만 응받받습니다.")
     @GetMapping
     public ResponseEntity<PagedModel<TrackResponse>> searchTrack(TrackSearchRequest searchRequest, @PageableDefault Pageable pageable) {
         Page<TrackResponse> trackResponses = trackService.searchTrack(searchRequest, pageable);
@@ -45,7 +50,8 @@ public class TrackController {
         return ResponseEntity.ok(new PagedModel<>(trackResponses));
     }
 
-    @RoleCheck(role = {UserRole.MASTER})
+    @Operation(summary = "배송 경로 완료", description = "배송 경로를 완료상태로 바꿉니다. Master 미만 권한은 본인이 포함된 리소스만 수정 가능합니다.")
+    @RoleCheck(role = {UserRole.MASTER, UserRole.MANAGER, UserRole.SHIPPER})
     @PatchMapping("/{trackId}/completion")
     public ResponseEntity<TrackResponse> completeTrack(@PathVariable UUID trackId,
                                                        @Valid @RequestBody TrackCompletionPatchRequest request) {
@@ -54,7 +60,8 @@ public class TrackController {
         return ResponseEntity.ok(trackResponse);
     }
 
-    @RoleCheck(role = {UserRole.MASTER})
+    @Operation(summary = "배송 경로 출발", description = "배송 경로를 배송중 상태로 바꿉니다. Master 미만 권한은 본인이 포함된 리소스만 수정 가능합니다.")
+    @RoleCheck(role = {UserRole.MASTER, UserRole.MANAGER, UserRole.SHIPPER})
     @PatchMapping("/{trackId}/departure")
     public ResponseEntity<TrackResponse> departureTrack(@PathVariable UUID trackId) {
         TrackResponse trackResponse = trackService.departureTrack(trackId);
