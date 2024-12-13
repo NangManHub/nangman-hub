33933cbdc6e8d4a3152c10f34ee3290ae187ba2e
권현준 <bonheur9813@naver.com>
2024-12-13 13:04:20 +0900

    refactor: FeignErrorDecoder 에러 처리 #59

--- user/src/main/java/com/nangman/user/common/exception/ExceptionType.java
+++ user/src/main/java/com/nangman/user/common/exception/ExceptionType.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionType {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-001", "서버 내부 오류입니다."),
+    FEIGN_CLIENT_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "F-001", "Feign Client 서버 내부 오류입니다."),
+
     DUPLICATE_USER_NAME(HttpStatus.BAD_REQUEST, "U-001", "중복된 아이디 입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U-002", "존재하지 않는 유저입니다."),
     PASSWORD_NOT_MATCHED(HttpStatus.BAD_REQUEST, "U-003", "일치하지 않는 비밀번호 입니다."),
@@ -16,7 +18,9 @@ public enum ExceptionType {
 
     ONLY_SHIPPER_REGISTERED(HttpStatus.BAD_REQUEST, "S-001", "SHIPPER로 등록된 사용자만 허용됩니다."),
     SHIPPER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "S-002", "해당 요청의 권한이 없습니다."),
-    SHIPPER_NOT_FOUND(HttpStatus.NOT_FOUND, "S-003", "해당하는 Shipper가 없습니다.");
+    SHIPPER_NOT_FOUND(HttpStatus.NOT_FOUND, "S-003", "해당하는 Shipper가 없습니다."),
+
+    HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "H-001", "허브가 존재하지 않습니다.");
 
     // 상태, 에러 코드, 메시지
     private final HttpStatus httpStatus;
