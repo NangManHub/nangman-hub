f89a1e86c02029260eef1334ffed94c3835f1c7c
POKUDING <yoseb95@gmail.com>
2024-12-15 16:43:46 +0900

    fix: ConsumerFactory<String,String> -> <UUID,ShipperMessage> 로 수정 #72

--- delivery/src/main/java/com/nangman/delivery/common/config/KafkaConfig.java
+++ delivery/src/main/java/com/nangman/delivery/common/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package com.nangman.delivery.common.config;
 
+import com.nangman.delivery.application.dto.kafka.ShipperMessage;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.UUIDDeserializer;
@@ -41,7 +43,7 @@ public class KafkaConfig {
     // ConsumerFactory는 Kafka 컨슈머 인스턴스를 생성하는 데 사용됩니다.
     // 각 컨슈머는 이 팩토리를 통해 생성된 설정을 기반으로 작동합니다.
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<UUID, ShipperMessage> consumerFactory() {
         // 컨슈머 팩토리 설정을 위한 맵을 생성합니다.
         Map<String, Object> configProps = new HashMap<>();
         // Kafka 브로커의 주소를 설정합니다.
@@ -58,9 +60,9 @@ public class KafkaConfig {
     // ConcurrentKafkaListenerContainerFactory는 Kafka 메시지를 비동기적으로 수신하는 리스너 컨테이너를 생성하는 데 사용됩니다.
     // 이 팩토리는 @KafkaListener 어노테이션이 붙은 메서드들을 실행할 컨테이너를 제공합니다.
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<UUID, ShipperMessage> kafkaListenerContainerFactory() {
         // ConcurrentKafkaListenerContainerFactory를 생성합니다.
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<UUID, ShipperMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
         // 컨슈머 팩토리를 리스너 컨테이너 팩토리에 설정합니다.
         factory.setConsumerFactory(consumerFactory());
         // 설정된 리스너 컨테이너 팩토리를 반환합니다.
