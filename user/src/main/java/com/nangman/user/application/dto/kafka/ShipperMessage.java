25f041fa8b2539c638dfb7314e9e6893508c677d
권현준 <bonheur9813@naver.com>
2024-12-15 12:11:54 +0900

    fix: ShipperMessage 필드명 수정 #77

--- user/src/main/java/com/nangman/user/application/dto/kafka/ShipperMessage.java
+++ user/src/main/java/com/nangman/user/application/dto/kafka/ShipperMessage.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public record ShipperMessage(
         String action,      // 메세지 타입(create/update/delete)
         UUID userId,        // 유저 PK
-        String usernmae,    // 유저 아이디
+        String username,    // 유저 아이디
         String name,        // 유저 이름
         UUID hubId,         // 소속 허브 PK
         ShipperType type    // 담당 분류
