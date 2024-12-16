5012ea8c357663f3f6d97a870cee7c1920b51759
권현준 <bonheur9813@naver.com>
2024-12-16 16:52:33 +0900

    feat: Slack 서비스 기본 세팅 #111

---
+++ slack/src/main/java/com/nangman/slack/common/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.nangman.slack.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private ExceptionType exceptionType;
+
+    public CustomException(ExceptionType exceptionType){
+        super(exceptionType.getMessage());
+        this.exceptionType = exceptionType;
+    }
+}
