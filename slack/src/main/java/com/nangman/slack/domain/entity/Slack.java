fc76733c4fa16f5c262571660ad923be92e18974
권현준 <bonheur9813@naver.com>
2024-12-16 16:53:44 +0900

    feat: Slack 서비스 엔티티 생성 #111

---
+++ slack/src/main/java/com/nangman/slack/domain/entity/Slack.java
@@ -0,0 +1,33 @@
+package com.nangman.slack.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_slacks")
+public class Slack extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "message", nullable = false, columnDefinition="TEXT")
+    private String message;
+
+    @Column(name = "send_at", nullable = false)
+    private LocalDateTime sendAt;
+
+    @Column(name = "receiver_id", nullable = false, length = 10)
+    private String receiverId;
+
+}
