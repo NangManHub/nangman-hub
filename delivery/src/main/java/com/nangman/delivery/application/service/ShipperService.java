b63efa8c7cd26ec20eec51c459f233091eda0686
POKUDING <yoseb95@gmail.com>
2024-12-16 11:12:08 +0900

    fix: ShipperService 트랜잭션 추가 #72

--- delivery/src/main/java/com/nangman/delivery/application/service/ShipperService.java
+++ delivery/src/main/java/com/nangman/delivery/application/service/ShipperService.java
@@ -6,6 +6,7 @@ import com.nangman.delivery.domain.repository.ShipperRepository;
 import com.nangman.delivery.infrastructure.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,18 +15,21 @@ public class ShipperService {
     private final ShipperRepository shipperRepository;
     private final RedisService redisService;
 
+    @Transactional
     public void addShipper(ShipperMessage shipperMessage) {
         Shipper shipper = shipperRepository.save(shipperMessage.toEntity());
 
         redisService.addShipperZSet(shipper, 0);
     }
 
+    @Transactional
     public void updateShipper(ShipperMessage shipperMessage) {
         Shipper shipper = shipperRepository.getById(shipperMessage.userId());
 
         shipper.update(shipperMessage.hubId(), shipperMessage.type());
     }
 
+    @Transactional
     public void deleteShipper(ShipperMessage shipperMessage) {
         Shipper shipper = shipperRepository.getById(shipperMessage.userId());
 
