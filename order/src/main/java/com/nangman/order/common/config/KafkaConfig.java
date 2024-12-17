1d37b438dafa85605ae239c62141b06054113c63
POKUDING <yoseb95@gmail.com>
2024-12-17 17:25:41 +0900

    refactor: application.yml dev,prod 분리 #130

--- order/src/main/java/com/nangman/order/common/config/KafkaConfig.java
+++ order/src/main/java/com/nangman/order/common/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
